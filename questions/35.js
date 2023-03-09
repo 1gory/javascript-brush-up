4e2f6b56132e6fbb64bde685360330f6dd89886b
Igor Pershin <igor.pershin.me@gmail.com>
2023-03-09 10:24:31 +0300

    add one more question

---
+++ questions/35.js
@@ -0,0 +1,16 @@
+// Существую ли приватные свойства в JS?
+
+class myClass {
+  #privateProperty = 100;
+
+  getPrivateProperty() {
+    console.log(this.#privateProperty);
+  }
+}
+
+const obj = new myClass();
+
+obj.getPrivateProperty(); // 100
+
+// SyntaxError: Private field '#privateProperty' must be declared in an enclosing class
+console.log(obj.#privateProperty);
