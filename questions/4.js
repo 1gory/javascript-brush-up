d9eae58446c3057c28964cd95b4783a5d86dd9b2
Ig <igor.pershin.me@gmail.com>
2024-06-27 16:39:42 +0300

    Update 4.js

--- questions/4.js
+++ questions/4.js
@@ -1,15 +1,15 @@
-// Как создать объект Map из обычного объекта?
+// How to create a Map object from a regular object?
 const obj = {
   name: 'Igor Pershin',
   age: 30
 };
 
-// Помимо добавелния в коллекцию методом map.set('name', 'Igor Pershin'),
-// при мы можем инициализации передать массив пар [ключ, значение].
-// Object.entries(obj) как раз вернет [['name', 'Igor Pershin'], ['age', '30']]
+// Besides adding to the collection with map.set('name', 'Igor Pershin'),
+// we can also initialize it by passing an array of key-value pairs.
+// Object.entries(obj) will return [['name', 'Igor Pershin'], ['age', '30']].
 const map = new Map(Object.entries(obj));
 console.log(map.get('name')); // Igor Pershin
 
-// Таким же образом мы можем в обратную сторону из коллекции Map создать объект
+// Similarly, we can reverse this process to create a regular object from a Map collection.
 const newObj = Object.fromEntries(map.entries());
 console.log(newObj); // { name: 'Igor Pershin', age: 30 }
