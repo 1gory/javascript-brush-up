6240381f162214d26c73a767fb19eec4e892275e
Ig <igor.pershin.me@gmail.com>
2024-06-27 18:37:52 +0300

    Update 25.js

--- questions/25.js
+++ questions/25.js
@@ -1,12 +1,12 @@
-// Что делает метод toLocaleString?
+// What does the toLocaleString method do?
 
-// 1. позволяет привести дату к строке нужного формата
+// 1. It allows converting a date to a string in the desired format.
 console.log(new Date().toLocaleString('en-US')); // 2/26/2024, 11:49:32 PM
 console.log(new Date().toLocaleString('ru-RU')); // 26.02.2023, 23:49:52
 
-// 2. позволяет привести число к строке нужного формата
+// 2. It allows converting a number to a string in the desired format.
 console.log(10000000..toLocaleString()); // 10,000,000
 console.log(10000000..toLocaleString('ru-RU')); // 10 000 000
 
-// так же есть аргумент options, который позволяет отредактировать полученный результат
+// There is also an options argument that allows editing the resulting output.
 console.log(100..toLocaleString('ru-RU', {style: 'currency', currency: 'RUB'})); // 100,00 ₽
