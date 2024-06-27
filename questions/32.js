cc263f520825484bfebada6b2fac7f3c98df948a
Ig <igor.pershin.me@gmail.com>
2024-06-27 18:59:56 +0300

    Update 32.js

--- questions/32.js
+++ questions/32.js
@@ -1,10 +1,10 @@
-// Что произойдет, если обратиться к переменной до объявления?
+// What happens if a variable is accessed before it is declared?
 
-// если переменная объявлена устаревшим ключевым словом var, она будет равна undefined,
-// этот процесс описывается термином поднятие/всплыте (hoisting)
+// if a variable is declared with the deprecated var keyword, it will be undefined,
+// this process is described by the term hoisting
 console.log(one); // undefined
 var one = 1;
 
-// если переменная объявлена с ключевыми словами let/const, будет ошибка
+// if the variable is declared with let/const keywords, there will be an error
 console.log(two); // Cannot access 'two' before initialization
 const two = 1;
