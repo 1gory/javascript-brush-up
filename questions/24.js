6fcbb2afc395bfcc685d72e120e17a495d0625cc
Ig <igor.pershin.me@gmail.com>
2024-06-27 18:36:00 +0300

    Update 24.js

--- questions/24.js
+++ questions/24.js
@@ -1,13 +1,13 @@
-// Что такое мутирующие и немутирующие методы на примере методов массивов?
+// What are mutating and non-mutating methods using array methods as examples?
 
-// Это методы, которые меняют массив или объект, к которому были прменены, к примеру:
+// These are methods that modify the array or object to which they are applied, for example:
 const arr = [1, 2, 3, 4, 5, 6];
 
-console.log(arr.pop()); // 6 - получен последний элемент массива
-console.log(arr); // [ 1, 2, 3, 4, 5 ] - исходный массив изменен
+console.log(arr.pop()); // 6 - the last element of the array was obtained
+console.log(arr); // [ 1, 2, 3, 4, 5 ] - the original array has been modified
 
-console.log(arr.push(7)); // 6 - метод возврщает новую длинну массива
-console.log(arr); // [ 1, 2, 3, 4, 5, 7 ] - исходный массив опять изменен
+console.log(arr.push(7)); // 6 - the method returns the new length of the array
+console.log(arr); // [ 1, 2, 3, 4, 5, 7 ] - the original array has been modified again
 
-console.log(arr.splice(0, 3, 9, 8)); // [ 1, 2, 3 ] - вернет подмассив, который был удален
-console.log(arr); // [ 9, 8, 4, 5, 7 ] - новый массив, 9, 8 на месте удалленной части
+console.log(arr.splice(0, 3, 9, 8)); // [ 1, 2, 3 ] - will return the subarray that was removed
+console.log(arr); // [ 9, 8, 4, 5, 7 ] - a new array with 9 and 8 in place of the removed part
