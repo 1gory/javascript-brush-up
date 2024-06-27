d6259391a26ce47a4a802db2cdba492479144566
Ig <igor.pershin.me@gmail.com>
2024-06-27 19:16:26 +0300

    Update 39.js

--- questions/39.js
+++ questions/39.js
@@ -1,1 +1,10 @@
-// В чем разница между "&&" и "??" ?
+// What is the difference between "&&" and "??" ?
+
+// The ?? operator is used to check for null or undefined. 
+// If the operand on the left side of ?? is null or undefined, it returns the right operand. Here's how it works:
+
+// Syntax: expr1 ?? expr2
+
+
+// || is used for providing a fallback value when the left operand is falsy (values that convert to false in a boolean context).
+// ?? is used specifically for providing a default value when the left operand is null or undefined
