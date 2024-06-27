0082e8f47e8ed50606549ecd25398265904afcdf
Ig <igor.pershin.me@gmail.com>
2024-06-27 18:15:36 +0300

    Update 19.js

--- questions/19.js
+++ questions/19.js
@@ -1,4 +1,5 @@
-// value - само значение свойства;
-// writable – если true, свойство можно изменить, иначе оно только для чтения;
-// enumerable – если true, свойство перечисляется в циклах, в противном случае циклы его игнорируют;
-// configurable – если true, свойство можно удалить, а эти атрибуты можно изменять, иначе этого делать нельзя;
+// Flags and property descriptors
+// value - the value of the property
+// writable – if true, the property can be modified; otherwise, it is read-only
+// enumerable – if true, the property is enumerable, otherwise, loops will ignore it
+// configurable – f true, the property can be deleted, and these attributes can be changed; otherwise, they cannot
