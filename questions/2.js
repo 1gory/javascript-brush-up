3cd54c19037f376d4e699600e6522a0f7e219cb9
Ig <igor.pershin.me@gmail.com>
2024-06-27 16:22:52 +0300

    Update 2.js

--- questions/2.js
+++ questions/2.js
@@ -1,39 +1,35 @@
-// Как перебрать свойства в объекте?
+// How to iterate over the properties in an object?
 let obj = {
   name: 'Igor Pershin',
   age: 30,
   isAdmin: true,
 };
 
-// 1. С помощью конструкции for (let key in obj)
-// Перебираются как собственные, так и унаследованные свойства.
-// Унаследованные можно отфильтровать с помощью obj.hasOwnProperty
-// Не перебираются свойства, с установленным дескриптором enumerable: false
+// 1. Using the `for (let key in obj)` construct
+// Both own and inherited properties are iterated over.
+// Inherited properties can be filtered out using `obj.hasOwnProperty`.
+// Properties with the `enumerable: false` descriptor are not iterated over.
 for (let key in obj) {
   console.log(key);  // name, age, isAdmin
   console.log(obj[key]); // Igor Pershin, 30, true
 }
 
-// 2. Получить нужные ключи с помощью методов
-// и перебрать их с помощью встроенных методов массива
-
-// Object.getOwnPropertyNames(obj) - получить массив
-// с именами собственных (не наследуемых) свойствами.
-// Дескриптор enumerable не имеет значения.
+// 2. To get the desired keys using methods and iterate over them using array built-in methods:
+// Object.getOwnPropertyNames(obj) retrieves an array containing the names of the object's own (non-inherited) properties.
+// The `enumerable` descriptor does not affect the result.
 Object.getOwnPropertyNames(obj).forEach((propertyName) => {
   console.log(propertyName); // name, age, isAdmin
   console.log(obj[propertyName]); // Igor Pershin, 30, true
 });
 
-// получить массив символьных ключей Object.getOwnPropertySymbols(obj)
+// To obtain an array of symbol keys with Object.getOwnPropertySymbols(obj)
 obj[Symbol('id')] = 'SymbolValue';
 Object.getOwnPropertySymbols(obj).forEach((propertyName) => {
   console.log(propertyName); // Symbol(id)
   console.log(obj[propertyName]); // SymbolValue
 });
 
-// Object.keys/values() - возвращает собственные (не наследуемые)
-// несимвольные ключи/значения с флагом enumerable: true
+// Object.keys/values() - Returns own (non-inherited), non-symbol keys/values with the enumerable flag set to true
 Object.keys(obj).forEach((key) => {
   console.log(key); // name, age, isAdmin
 });
