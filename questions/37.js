32863323c376845aa1b7a79b824617b0be256e8d
Ig <igor.pershin.me@gmail.com>
2024-06-27 19:08:04 +0300

    Update 37.js

--- questions/37.js
+++ questions/37.js
@@ -1,1 +1,4 @@
-// Как проверить, является ли переменная булевым значением true?
+// How to check if a variable is a boolean value true?
+
+// let myBool = true;
+// let myBool = Boolean(myVar); myBool === true
