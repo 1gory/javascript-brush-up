1ea26d83b399101fdd91a1f8e6a5a7c14e81c70a
Ig <igor.pershin.me@gmail.com>
2024-06-27 19:11:55 +0300

    Update 38.js

--- questions/38.js
+++ questions/38.js
@@ -1,1 +1,8 @@
-// Как работает Number? Что вернет Number([])?
+// How does Number work? What will Number([]) return?
+
+console.log(Number([])); // Output: 0
+console.log(Number({})); // Output: NaN
+console.log(Number(undefined)); // NaN
+console.log(Number(NaN)); // NaN
+
+// The other types are intuitive.
