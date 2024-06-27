2fc7150dfca8bd5d954f173fceefe100ca30dcb6
Ig <igor.pershin.me@gmail.com>
2024-06-27 17:39:23 +0300

    Update 13.js

--- questions/13.js
+++ questions/13.js
@@ -1,12 +1,12 @@
-// Как получить прототип объекта?
+// How to get the prototype of an object?
 
-// 1. Через геттер (и сеттер) __proto__. Устаревший способ, но всё еще рабочий.
+// 1. Using getter (and setter) __proto__. This is an outdated method, but still functional.
 const obj = {};
 console.log(obj.__proto__);
 
-// 2. Более современный способ Object.getPrototypeOf(obj)
+// 2. A more modern way is `Object.getPrototypeOf(obj)`.
 console.log(Object.getPrototypeOf(obj));
 
-// 3. Через свойство prototype функции-конструктора.
-// Сработает, если это свойство установлено корректно и не модифицировалось.
+// 3. Using the prototype property of a constructor function.
+// It works if this property is correctly set and has not been modified.
 console.log(obj.constructor.prototype);
