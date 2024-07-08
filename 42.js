b031a2938b60caac6dbface186c6e0941e26560d
Ig <igor.pershin.me@gmail.com>
2024-07-08 09:17:27 +0300

    Create 42.js

---
+++ 42.js
@@ -0,0 +1,7 @@
+// What the differences between __proto__, prototype и [[Prototype]]?
+
+// __proto__ is getter or setter to set or get a prototype
+
+// prototype is a special property for the constructor function
+
+// [[Prototype]] is an interal and hidden property, that links to a prototype
